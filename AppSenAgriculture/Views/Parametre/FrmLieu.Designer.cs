bdf79c96608fcee3538d15b70e48454da6b32e8e
kaba476 <akeita476@gmail.com>
2026-03-09 20:10:56 +0000

    Ajout du formulaire Lieu

--- AppSenAgriculture/Views/Parametre/FrmLieu.Designer.cs
+++ AppSenAgriculture/Views/Parametre/FrmLieu.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace AppSenAgriculture.Views.Parametre
+namespace AppSenAgriculture.Views.Parametre
 {
     partial class FrmLieu
     {
         /// <summary>
-        /// Required designer variable.
+        /// Variable requise par le concepteur.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Libère les ressources utilisées.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true pour libérer les ressources managées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -23,25 +23,150 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Méthode requise pour le support du concepteur - ne pas modifier
+        /// le contenu de cette méthode avec l’éditeur de code.
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgLieux = new System.Windows.Forms.DataGridView();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.lblAdresse = new System.Windows.Forms.Label();
+            this.txtAdresse = new System.Windows.Forms.TextBox();
+            this.btnSelectionner = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgLieux)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgLieux
+            // 
+            this.dgLieux.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgLieux.Location = new System.Drawing.Point(283, 32);
+            this.dgLieux.Margin = new System.Windows.Forms.Padding(2);
+            this.dgLieux.Name = "dgLieux";
+            this.dgLieux.RowHeadersWidth = 62;
+            this.dgLieux.RowTemplate.Height = 28;
+            this.dgLieux.Size = new System.Drawing.Size(583, 436);
+            this.dgLieux.TabIndex = 0;
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(27, 44);
+            this.lblNom.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(62, 13);
+            this.lblNom.TabIndex = 1;
+            this.lblNom.Text = "Nom du lieu";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(29, 65);
+            this.txtNom.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(233, 20);
+            this.txtNom.TabIndex = 2;
+            // 
+            // lblAdresse
+            // 
+            this.lblAdresse.AutoSize = true;
+            this.lblAdresse.Location = new System.Drawing.Point(27, 112);
+            this.lblAdresse.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAdresse.Name = "lblAdresse";
+            this.lblAdresse.Size = new System.Drawing.Size(45, 13);
+            this.lblAdresse.TabIndex = 3;
+            this.lblAdresse.Text = "Adresse";
+            // 
+            // txtAdresse
+            // 
+            this.txtAdresse.Location = new System.Drawing.Point(29, 133);
+            this.txtAdresse.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAdresse.Multiline = true;
+            this.txtAdresse.Name = "txtAdresse";
+            this.txtAdresse.Size = new System.Drawing.Size(233, 103);
+            this.txtAdresse.TabIndex = 4;
+            // 
+            // btnSelectionner
+            // 
+            this.btnSelectionner.Location = new System.Drawing.Point(184, 26);
+            this.btnSelectionner.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSelectionner.Name = "btnSelectionner";
+            this.btnSelectionner.Size = new System.Drawing.Size(77, 25);
+            this.btnSelectionner.TabIndex = 5;
+            this.btnSelectionner.Text = "&Selectionner";
+            this.btnSelectionner.UseVisualStyleBackColor = true;
+            this.btnSelectionner.Click += new System.EventHandler(this.btnSelectionner_Click);
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(184, 260);
+            this.btnAjouter.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(77, 21);
+            this.btnAjouter.TabIndex = 6;
+            this.btnAjouter.Text = "&Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(184, 294);
+            this.btnModifier.Margin = new System.Windows.Forms.Padding(2);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(77, 21);
+            this.btnModifier.TabIndex = 7;
+            this.btnModifier.Text = "&Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(184, 328);
+            this.btnSupprimer.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(77, 21);
+            this.btnSupprimer.TabIndex = 8;
+            this.btnSupprimer.Text = "S&upprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
             // 
             // FrmLieu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(877, 476);
             this.ControlBox = false;
+            this.Controls.Add(this.btnSupprimer);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.btnSelectionner);
+            this.Controls.Add(this.txtAdresse);
+            this.Controls.Add(this.lblAdresse);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.dgLieux);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmLieu";
-            this.Text = "FrmLieu";
+            this.Text = "Lieu";
+            this.Load += new System.EventHandler(this.FrmLieu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgLieux)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgLieux;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label lblAdresse;
+        private System.Windows.Forms.TextBox txtAdresse;
+        private System.Windows.Forms.Button btnSelectionner;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnSupprimer;
     }
 }
--- AppSenAgriculture/Views/Parametre/FrmLieu.cs
+++ AppSenAgriculture/Views/Parametre/FrmLieu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using AppSenAgriculture.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,89 @@ namespace AppSenAgriculture.Views.Parametre
 {
     public partial class FrmLieu : Form
     {
+        // Contexte de la base de donnee
+        BdSenAgricultureContext db = new BdSenAgricultureContext();
+
         public FrmLieu()
         {
             InitializeComponent();
+        }
+
+        // Au chargement du formulaire 
+        private void FrmLieu_Load(object sender, EventArgs e)
+        {
+            ChargerLieux();
+        }
+
+        // Charge les lieux 
+        private void ChargerLieux()
+        {
+            dgLieux.DataSource = db.Lieux.ToList();
+        }
+
+        // Vide les champs 
+        private void EffacerChamps()
+        {
+            txtNom.Clear();
+            txtAdresse.Clear();
+            ChargerLieux();
+            txtNom.Focus();
+        }
+
+        // Ajoute 
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            Lieu lieu = new Lieu()
+            {
+                NomLieu = txtNom.Text,
+                AdresseLieu = txtAdresse.Text
+            };
+
+            db.Lieux.Add(lieu);
+            db.SaveChanges();
+            EffacerChamps();
+        }
+
+        // Modifie 
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            if (dgLieux.CurrentRow == null) return;
+
+            int id = int.Parse(dgLieux.CurrentRow.Cells[0].Value.ToString());
+            Lieu lieu = db.Lieux.Find(id);
+
+            if (lieu != null)
+            {
+                lieu.NomLieu = txtNom.Text;
+                lieu.AdresseLieu = txtAdresse.Text;
+                db.SaveChanges();
+                EffacerChamps();
+            }
+        }
+
+        // Supprime 
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            if (dgLieux.CurrentRow == null) return;
+
+            int id = int.Parse(dgLieux.CurrentRow.Cells[0].Value.ToString());
+            Lieu lieu = db.Lieux.Find(id);
+
+            if (lieu != null)
+            {
+                db.Lieux.Remove(lieu);
+                db.SaveChanges();
+                EffacerChamps();
+            }
+        }
+
+        
+        private void btnSelectionner_Click(object sender, EventArgs e)
+        {
+            if (dgLieux.CurrentRow == null) return;
+
+            txtNom.Text = dgLieux.CurrentRow.Cells[1].Value.ToString();
+            txtAdresse.Text = dgLieux.CurrentRow.Cells[2].Value.ToString();
         }
     }
 }
